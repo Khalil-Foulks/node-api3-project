1af0400b69fd92381d37ba2af632f2cd6275146e
Khalil <khalilfoulks@gmail.com>
2020-08-06 15:39:53 -0400

    made changes to allow for HEROKU deployment

--- server.js
+++ server.js
@@ -3,16 +3,19 @@ const customMw = require('./customMiddleware/customMiddleware')
 
 const server = express();
 
+//Environment Variable Here
+const name = process.env.NAME
+
 //Router import Here
 const userRouter = require('./users/userRouter')
 const postRouter = require('./posts/postRouter')
 
-//Global Middleware here
+//Global Middleware Here
 server.use(express.json());
 server.use(customMw.logger);
 
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+  res.send(`<h2> Hello ${name} Let's write some middleware!</h2>`);
 });
 
 server.use("/api/users", userRouter)
